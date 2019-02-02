923bb9b2ff9040599fd56976401d8ac767864328
Lindroy <linyulong2014@163.com>
2019-02-02 17:09:11 +0800

    给HintDialog添加设置动画和主题样式的函数。

--- app/src/main/java/com/lindroid/lindialog/MainActivity.kt
+++ app/src/main/java/com/lindroid/lindialog/MainActivity.kt
@@ -2,17 +2,51 @@ package com.lindroid.lindialog
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.lindroid.lindialog_lib.HintDialog
+import com.lindroid.utils.shortToast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnHint.setOnClickListener {
-            HintDialog.build(supportFragmentManager)
-                    .show()
+        btnHint.setOnClickListener(this)
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnHint -> showHintDialog()
         }
+    }
+
+    private fun showHintDialog() {
+        HintDialog.build(supportFragmentManager)
+//                .setThemeStyle(R.style.HintDialogStyle)
+                .setTitle(R.string.app_name)
+                .setMessage("这是一个提示对话框")
+                .setShowNeuButton(true)
+                .setNeutralText("不确定")
+                .setCanCancel(true)
+                .setCanDismiss(false)
+                .setPosClickListener {
+                    shortToast("确定")
+                    it.dismiss()
+                }
+                .setNegClickListener {
+                    shortToast("取消")
+                    it.dismiss()
+                }
+                .setNeuClickListener {
+                    shortToast("不确定")
+                    it.dismiss()
+                }
+                .show()
     }
 }
--- lindialog_lib/src/main/java/com/lindroid/lindialog_lib/HintDialog.kt
+++ lindialog_lib/src/main/java/com/lindroid/lindialog_lib/HintDialog.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.support.annotation.ColorInt
 import android.support.annotation.ColorRes
 import android.support.annotation.StringRes
+import android.support.annotation.StyleRes
 import android.support.v4.app.DialogFragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.content.ContextCompat
@@ -32,16 +33,17 @@ class HintDialog : DialogFragment() {
     private var posColor = 0
     private var showNeuButton = false
     private var showNegButton = true
-    private var dismissable = true
+    private var dismissible = true
     private var dialogTag = "HintDialog"
+    private var themeStyle = 0
+    private var animStyle = 0
     private var posListener: ((DialogInterface) -> Unit)? = null
     private var negListener: ((DialogInterface) -> Unit)? = null
     private var neuListener: ((DialogInterface) -> Unit)? = null
     private var dismissListener: (() -> Unit)? = null
 
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val builder = context?.let { AlertDialog.Builder(it) }
+        val builder = context?.let { AlertDialog.Builder(it, themeStyle) }
         builder?.apply {
             setTitle(title)
             setMessage(message)
@@ -59,7 +61,7 @@ class HintDialog : DialogFragment() {
                 setTextColor(if (posColor != 0) posColor else ContextCompat.getColor(context, R.color.hint_dialog_positive_text_color))
                 setOnClickListener {
                     posListener?.invoke(dialog)
-                    if (dismissable) {
+                    if (dismissible) {
                         dismiss()
                     }
                 }
@@ -68,16 +70,16 @@ class HintDialog : DialogFragment() {
                 setTextColor(if (negColor != 0) negColor else ContextCompat.getColor(context, R.color.hint_dialog_negative_text_color))
                 setOnClickListener {
                     negListener?.invoke(dialog)
-                    if (dismissable) {
+                    if (dismissible) {
                         dismiss()
                     }
                 }
             }
             getButton(AlertDialog.BUTTON_NEUTRAL).apply {
-                setTextColor(neuColor)
+                setTextColor(if (neuColor != 0) neuColor else ContextCompat.getColor(context, R.color.hint_dialog_neutral_text_color))
                 setOnClickListener {
                     neuListener?.invoke(dialog)
-                    if (dismissable) {
+                    if (dismissible) {
                         dismiss()
                     }
                 }
@@ -95,8 +97,17 @@ class HintDialog : DialogFragment() {
                     posText = mContext.getString(R.string.hint_dialog_positive_text)
                     negText = mContext.getString(R.string.hint_dialog_negative_text)
                     neuText = mContext.getString(R.string.hint_dialog_neutral_text)
-//                    Log.e("Hint","context=$mContext")
                 }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog.window?.setWindowAnimations(animStyle)
+    }
+
+    override fun onDismiss(dialog: DialogInterface?) {
+        super.onDismiss(dialog)
+        dismissListener?.invoke()
     }
 
     fun show() {
@@ -104,6 +115,8 @@ class HintDialog : DialogFragment() {
     }
 
     fun setTitle(title: String) = this.apply { this.title = title }
+
+    fun setTitle(@StringRes id: Int) = this.apply { setTitle(mContext.getString(id)) }
 
     fun setMessage(msg: String) = this.apply { message = msg }
 
@@ -127,15 +140,21 @@ class HintDialog : DialogFragment() {
 
     fun setTag(tag: String) = this.apply { dialogTag = tag }
 
-    fun setCanDissmiss(dismissable: Boolean) = this.apply { this.dismissable = dismissable }
 
-    fun setPosClickListener(listener: (DialogInterface) -> Unit) = this.apply { posListener = listener }
+    /**
+     * 点击对话框上的按钮是否可以关闭对话框，默认为true
+     */
+    fun setCanDismiss(dismissible: Boolean) = this.apply { this.dismissible = dismissible }
 
-    fun setNegClickListener(listener: (DialogInterface) -> Unit) = this.apply { negListener = listener }
+    /**
+     * 设置对话框的主题样式
+     */
+    fun setThemeStyle(@StyleRes styleId: Int) = this.apply { themeStyle = styleId }
 
-    fun setNeuClickListener(listener: (DialogInterface) -> Unit) = this.apply { neuListener = listener }
-
-    fun setDismissListener(listener: () -> Unit) = this.apply { dismissListener = listener }
+    /**
+     * 设置对话框的动画样式
+     */
+    fun setAnimStyle(@StyleRes styleId: Int) = this.apply { animStyle = styleId }
 
     fun setNeuTextColor(@ColorInt color: Int) = this.apply { neuColor = color }
 
@@ -153,6 +172,13 @@ class HintDialog : DialogFragment() {
     fun setPosTextColorId(@ColorRes colorId: Int) =
             this.apply { setPosTextColor(ContextCompat.getColor(mContext, colorId)) }
 
+    fun setPosClickListener(listener: (DialogInterface) -> Unit) = this.apply { posListener = listener }
+
+    fun setNegClickListener(listener: (DialogInterface) -> Unit) = this.apply { negListener = listener }
+
+    fun setNeuClickListener(listener: (DialogInterface) -> Unit) = this.apply { neuListener = listener }
+
+    fun setDismissListener(listener: () -> Unit) = this.apply { dismissListener = listener }
     /**
      * 点击对话框外部关闭对话框
      */
